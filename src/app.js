1c6c279670c40bbfb4b8eb9a1cba351e72a55db6
James Byrne <james.c.byrne1994@gmail.com>
2023-01-26 20:27:45 +0000

    core criteria complete

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import Header from './components/header'
+import { useState } from 'react'
 
 import initialEmails from './data/emails'
 
@@ -7,6 +8,8 @@ import './styles/app.css'
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+
+  const [emails, setEmails] = useState(initialEmails)
 
   return (
     <div className="app">
@@ -29,7 +32,7 @@ function App() {
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
@@ -39,7 +42,40 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {
+          emails.map((obj) => {
+            return (
+
+              <ul>
+                  <li className={obj.read ? 'email read' : 'email unread'} key={obj.id }>
+                    <div className="select">
+                    <input type="checkbox"/>
+                    </div>
+                    <div className="star">
+                    <input className="star-checkbox" type="checkbox" checked={obj.starred} />
+                    </div>
+                    <div className="sender">
+                      {obj.sender}
+                    </div>
+                  
+                  <div className='title '>
+                    {obj.title}
+                  </div>
+                  </li>
+                </ul>
+            )
+                      
+            //console.log("here is item" , obj )
+          })
+          
+        }
+      
+        
+        {/* Render a list of emails here */}
+        
+        
+        </main>
     </div>
   )
 }
